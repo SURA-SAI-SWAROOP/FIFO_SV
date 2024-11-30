9cdd84e847bd752b005fcc7b19fc5cc1f794388d
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2024-11-30 10:21:40 +0530

    generator.sv

--- generator.sv
+++ generator.sv
@@ -1,20 +1,25 @@
 class generator;
-transaction tr;
-mailbox #(transaction) gen2drv;
-event sconxt;
-event gen_ended;
-int trans_count;
-function new (mailbox #(transaction) gen2drv);
-this.gen2drv=gen2drv;
-tr=new();
-endfunction
-task main();
-repeat (trans_count) begin
-assert(tr.randomize()) else $error("[GEN]:: RANDOMIZATION FAILED");
-gen2drv.put(tr.copy());
-tr.display("GEN");
-@(sconxt);
-end
-->gen_ended;
-endtask
+  transaction tr;
+  mailbox #(transaction) gen2drv;
+  
+  event sconxt;
+  event gen_ended;
+  
+  int trans_count;
+  
+  function new (mailbox #(transaction) gen2drv);
+    this.gen2drv=gen2drv;
+    tr=new();
+  endfunction
+    
+  task main();
+    repeat (trans_count) begin
+      assert(tr.randomize()) else $error("[GEN]:: RANDOMIZATION FAILED");
+      gen2drv.put(tr.copy());
+      tr.display("GEN");
+      @(sconxt);
+    end
+    ->gen_ended;
+  endtask
+    
 endclass
